b0f038c05dfcd25565cc92ecd74aedceef6d31d4
Kurt Jeffries <elrordoomhammer@gmail.com>
2023-02-08 16:02:48 -0500

    first jsx

---
+++ MERN/react/intro/first_jsx/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
