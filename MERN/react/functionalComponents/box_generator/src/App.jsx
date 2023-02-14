f5a91f00f030dab10d72c42a26b834d54b5c04bf
Kurt Jeffries <elrordoomhammer@gmail.com>
2023-02-13 21:30:03 -0500

    box generator

---
+++ MERN/react/functionalComponents/box_generator/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BoxForm from './components/BoxForm';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/react/functionalComponents/box_generator/src/components/BoxForm.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+
+const BoxForm = props => {
+    const [boxs, setBoxs] = useState([{color:"red",size:200}]);
+    const [color, setColor] = useState('');
+    const [size, setSize] = useState();
+
+    const submitBox = (e) => {
+        e.preventDefault();
+        const numSize = parseInt(size);
+        setBoxs([...boxs, {color,size:numSize}])
+
+        setColor('');
+        setSize('');
+    }
+
+    return (
+        <>
+            <form action="">
+                <div className="row">
+                    <label htmlFor="color">Color:</label>
+                    <input type="text" name="color" id="color" value={color} onChange={(e) => setColor(e.target.value)}/>
+                </div>
+                <div className="row">
+                    <label htmlFor="size">Size in px:</label>
+                    <input type="number" name="size" id="size" value={size} onChange={(e) => setSize(e.target.value)}/>
+                </div>
+                <div className="row">
+                    <button onClick={(e) => submitBox(e)}>Submit</button>
+                </div>
+            </form>
+            <hr />
+            <div className="boxes">
+                {boxs.map((box, i) => {
+                    return (
+                    <div className="box" key={i} style={{background:box.color, height:box.size, width:box.size}}></div>)
+                })}
+            </div>
+        </>
+    );
+}
+
+export default BoxForm;
