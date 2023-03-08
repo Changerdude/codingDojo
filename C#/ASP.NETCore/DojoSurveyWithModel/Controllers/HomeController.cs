ad3e7a067a456024f98bcdad73816264ebf4cf89
Kurt Jeffries <elrordoomhammer@gmail.com>
2023-03-08 17:50:11 -0500

    Dojo Survey with Model

---
+++ C#/ASP.NETCore/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("/submit")]
+    public RedirectToActionResult Submit(FormData Data){
+        if(Data.Comment == null) Data.Comment = "No Comments";
+        return RedirectToAction("Details", Data);
+    }
+
+    [HttpGet("/details")]
+    public ViewResult Details(FormData Data){
+        return View(Data);
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C#/ASP.NETCore/DojoSurveyWithModel/Models/FormData.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace DojoSurveyWithModel.Models;
+public class FormData
+{
+    public string Name {get;set;}
+    public string Location {get;set;}
+    public string Language {get;set;}
+    public string Comment {get;set;}
+
+}
