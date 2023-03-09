6228770a125ba7fbdb18206d64ee645dc0c83723
Kurt Jeffries <elrordoomhammer@gmail.com>
2023-03-09 13:34:27 -0500

    Date Validator

---
+++ C#/ASP.NETCore/DateValidator/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DateValidator.Models;
+
+namespace DateValidator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpPost("/submit")]
+    public IActionResult Submit(FormData Data){
+        if(ModelState.IsValid){
+            if(Data.Comment == null) Data.Comment = "No Comments";
+            return RedirectToAction("Index", Data);
+        } else {
+            return View("Index");
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C#/ASP.NETCore/DateValidator/Models/PrevDateAttribute.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+namespace DateValidator.Models;
+
+public class PrevDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)    
+    {
+        string DateString = (string)value;
+        string[] DateVals = DateString.Split('-');
+        DateTime CheckedDate = new DateTime(Int32.Parse(DateVals[0]), Int32.Parse(DateVals[1]), Int32.Parse(DateVals[2]), 8, 00, 00);
+        DateTime CurrentTime = DateTime.Now;
+        if (DateTime.Compare(CheckedDate,CurrentTime) > 0)
+        {
+            return new ValidationResult("Date cant be past today!");
+        } else {
+
+            return ValidationResult.Success;
+        }
+    }
+}
